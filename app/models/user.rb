8f6e3565c4ae345b32d929d0e91b60dbea3690b3
unknown <ibr.s.ahmat@gmail.com>
2022-12-24 17:00:31 +0300

    Add scholarships routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :scholarships, dependent: :destroy
+
   validates :password, presence: true
   validates :fullname, :email, :username, uniqueness: true, presence: true
 end
